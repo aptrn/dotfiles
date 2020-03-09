c680de23cf99099645c8f8f3f80f680053738e9f
aptrn <alessandro.nge@gmail.com>
2020-03-09 18:24:43 +0000

    forgot a copule of confs

--- install.sh
+++ install.sh
@@ -11,26 +11,29 @@ echo "copying confs"
 cp -r ./openbox/ ~/.config
 cp -r ./tint2/ ~/.config
 cp -r ./volti/ ~/.config
+cp -r ./conky/ ~/.config
 cp -r ./tema/Triste-Crimson/ ~/.themes
 cp -r ./.fonts ~/
 cp -r ./.icons ~/
 cp -r ./.urxvt ~/
 cp -r ./lightdm /etc/
 cp .Xdefaults ~/
+cp compton.conf ~/.config
 cp monsters.jpg ~/Pictures/
 echo "installing telegram"
 sudo add-apt-repository ppa:atareao/telegram
-sudo apt update && sudo apt install telegram
+sudo apt update
+sudo apt install telegram -y
 echo "installing spotify"
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install spotify-client -y
 echo "installing vscode"
-apt install software-properties-common apt-transport-https wget
+apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
-sudo apt install code
+sudo apt install code -y
 echo "enabling wirless"
 ifconfig wlp3s0 up
 echo "disable wait-online service"
