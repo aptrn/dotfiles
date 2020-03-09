7f4c99aac0e8e0ca3223e4d1ec103c179fb6bc40
aptrn <alessandro.nge@gmail.com>
2020-03-09 18:03:50 +0000

    add change shell

--- non-sudo.sh
+++ non-sudo.sh
@@ -1,5 +1,7 @@
 echo "set pulseaudio"
 pulseaudio -D
+echo "change shell to zsh"
+chsh -s /usr/bin/zsh
 echo "you should now set your openbox theme (Triste Crimson)"
 obconf
 echo "you should now set your system theme (Adwaita-dark)"
