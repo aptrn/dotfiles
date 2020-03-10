811aee1ae6973665b0a68296f09557e4990cff21
aptrn <alessandro.nge@gmail.com>
2020-03-10 13:23:41 +0000

    add docker

---
+++ docker.sh
@@ -0,0 +1,7 @@
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+reboot
