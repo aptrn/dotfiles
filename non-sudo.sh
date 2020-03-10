e2b23f3874f62abb2fce6ede12b941a2ca0e9b96
aptrn <alessandro.nge@gmail.com>
2020-03-10 11:20:00 +0000

    fix sudo

--- non-sudo.sh
+++ non-sudo.sh
@@ -3,7 +3,7 @@ pulseaudio -D
 echo "change shell to zsh"
 chsh -s /usr/bin/zsh
 echo "you should now set your openbox theme (Triste Crimson)"
-obconf
+sudo obconf
 echo "you should now set your system theme (Adwaita-dark)"
 lxappearance
 echo "you should now set your wallpaper"
